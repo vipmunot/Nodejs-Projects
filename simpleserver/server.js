5e7e06b50104be6ab5606321b1c6ce4111211976
Vipul <vipmunot@gmail.com>
2017-08-17 11:43:38 -0400

    Simple Web Server Completed

--- simpleserver/server.js
+++ simpleserver/server.js
@@ -1,45 +1,52 @@
-const http = require('http');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
-const mimeTypes = {
-  "html":"text/html",
-  "jpeg":"image/jpeg",
-  "jpg":"image/jpg",
-  "png":"image/png",
-  "js":"image/javascript",
-  "css":"image/css"
-}
-http.createServer(function(req,res){
-  var uri = url.parse(req.url).pathname;
-  var filename = path.join(process.cwd(),unescape(uri));
-  console.log('Loading'+uri);
-  var stats;
+// Array of Mime Types
+var mimeTypes = {
+    "html" : "text/html",
+    "jpeg" : "image/jpeg",
+    "jpg"  : "image/jpeg",
+    "png"  : "image/png",
+    "js"   : "text/javascript",
+    "css"  : "text/css"
+};
 
-  try{
-      stats = fs.lstatSync(filename);
-  }catch(e){
-        res.writeHead(404,{'Content-Type':'text/plain'});
-        res.write('404 Not Found \n');
+// Create Server
+
+http.createServer(function(req, res){
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading ' + uri);
+    var stats;
+
+    try{
+        stats = fs.lstatSync(fileName);
+    } catch (e) {
+        res.writeHead(404, {'Content-type' : 'text/plain'});
+        res.write('404 Not Found\n');
         res.end();
         return;
-  }
-    if(stats.isFile()){
-      var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
-      res.writeHead(200,{'Content-Type':mimeType});
-      var fileStream = fs.createReadStream(filename);
-      fileStream.pipe(res);
-    } else if (stats.isDirectory()) {
-      res.writeHead(302,{
-        'Location': 'index.html'
-      });
-      res.end();
+    }
 
-    }else{
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.write('500 Internal Error\n');
-      res.end();
+    // Check if file/directory
+    if(stats.isFile()){
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+        res.writeHead(200, {'Content-type' : mimeType});
+
+        var fileStream = fs.createReadStream(fileName);
+
+        fileStream.pipe(res);
+    } else if(stats.isDirectory()){
+        res.writeHead(302, {
+            'Location' : 'index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {'Content-type' : 'text/plain'});
+        res.write('500 Internal Error');
+        res.end();
     }
 }).listen(3000);
 /*
